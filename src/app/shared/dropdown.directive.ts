a29c1b95028839fe3bd3d38aaf6a1fb82e8232a3
Dori <dorantdushi@gmail.com>
2021-12-28 13:21:59 +0100

    Directives changes

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+  selector:'[appDropdown]'
+})
+
+
+
+export class DropDownDirective{
+
+  @HostBinding('class.open')  isOpen = false;
+
+  @HostListener('click') toogleOpen(){
+
+      this.isOpen = !this.isOpen;
+
+    }
+}
+
