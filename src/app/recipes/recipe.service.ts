1eefa9db2546103a7d6d8f1b651c2341be886a6c
Dori <dorantdushi@gmail.com>
2022-01-13 10:21:02 +0100

    Login Page

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,20 +10,20 @@ export class RecipeService {
 
  recipesChanged = new Subject<Recipe[]>();
 
- // private  recipes : Recipe [] = [
- //    new Recipe('Main Meal','Mixed Potatos','https://www.acouplecooks.com/wp-content/uploads/2020/05/Grilled-Sweet-Potatoes-009.jpg',
- //                        [
- //                          new Ingredient('Meat',1),
- //                          new Ingredient('French Fries',10)
- //                        ]),
- //    new Recipe('OctoberFest Burger','Route 66','https://pbs.twimg.com/media/DxMGelcXQAAKU5T.jpg',
- //            [
- //              new Ingredient('Buns',2),
- //              new Ingredient('Meat',1)
- //            ])
- //  ];
+ private  recipes : Recipe [] = [
+    new Recipe('Main Meal','Mixed Potatos','https://www.acouplecooks.com/wp-content/uploads/2020/05/Grilled-Sweet-Potatoes-009.jpg',
+                        [
+                          new Ingredient('Meat',1),
+                          new Ingredient('French Fries',10)
+                        ]),
+    new Recipe('OctoberFest Burger','Route 66','https://pbs.twimg.com/media/DxMGelcXQAAKU5T.jpg',
+            [
+              new Ingredient('Buns',2),
+              new Ingredient('Meat',1)
+            ])
+  ];
 
-  private recipes : Recipe [] = [];
+  // private recipes : Recipe [] = [];
 
   constructor(private slServer: ShoppingListService){
 
