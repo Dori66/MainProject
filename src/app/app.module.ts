19e35787f533599335c4e997c8768b1a16f2b9f2
Dori <dorantdushi@gmail.com>
2022-01-14 14:40:41 +0100

    Invalid Alert Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import {AlertComponent} from "./shared/alert/alert.component";
+import {Placeholder} from "@angular/compiler/src/i18n/i18n_ast";
+import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 
 
 @NgModule({
@@ -36,8 +39,10 @@ import {AuthInterceptorService} from "./auth/auth-interceptor.service";
     DropDownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent,
-    LoadingSpinnerComponent
+     AuthComponent,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -47,6 +52,7 @@ import {AuthInterceptorService} from "./auth/auth-interceptor.service";
     HttpClientModule
   ],
   providers: [ShoppingListService,RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, ComponentFactoryResolver, OnDestroy, ViewChild} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {Router} from "@angular/router";
+import {AlertComponent} from "../shared/alert/alert.component";
+import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 
-export class AuthComponent{
+export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+
+  private closeSub: Subscription;
 
 
 
@@ -54,5 +58,32 @@ constructor(private authService: AuthService, private router: Router) {
       });
     authForm.reset();
   }
+
+
+
+
+  onHandleError(){
+  this.error = null;
+  }
+
+
+  // private showErrorAlert(mesasge: string){
+  //   const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+  //
+  //   const hostViewContainerRef = this.alertHost.viewContainerRef;
+  //   hostViewContainerRef.clear();
+  //
+  //    const componentRef =   hostViewContainerRef.createComponent(alertCmpFactory)
+  //
+  //
+  //   componentRef.instance.message = mesasge;
+  //   this.closeSub =componentRef.instance.close.subscribe(() => {
+  //       this.closeSub.unsubscribe();
+  //       hostViewContainerRef.clear();
+  //   });
+  // };
+
+
+
 
 }
